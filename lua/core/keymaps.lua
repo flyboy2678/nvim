061c37cf953fe88885aef5182f5f8b3a5aba42d0
Hope Tsunke <ttsunke15@gmail.com>
2025-02-21 17:53:12 +0200

    chore: add new plugins

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,24 +1,58 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.g.have_nerd_font = true
+
 -- Disable spacebar key's default behavior in Normal and Visual modes
-vim.keymap.set({ 'n', 'v'}, '<Space>', '<Nop>', { silent = true })
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
 local opts = { noremap = true, silent = true }
 
 -- save file
-vim.keymap.set('n', 'i', '<C-s>', '<cmd> w <CR>', opts)
+vim.keymap.set({ "n", "i" }, "<C-s>", "<cmd> w <CR>", opts)
 
 -- quit file
-vim.keymap.set('n', '<C-w>', '<cmd> q <CR>', opts)
+vim.keymap.set("n", "<C-w>", "<cmd> q <CR>", opts)
 
--- go to normal mode 
-vim.keymap.set('i', 'jj', '<Esc>', opts)
+-- go to normal mode
+vim.keymap.set("i", "jj", "<Esc>", opts)
 
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', opts, { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', opts, { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', opts, { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', opts, { desc = 'Move focus to the upper window' }) 
+-- Navigate between splits
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", opts)
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", opts)
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", opts)
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", opts)
 
 -- Vertical scroll and center
-vim.keymap.set('n', '<C-u>', '<C-u>zz', opts)
-vim.keymap.set('n', '<C-d>', '<C-d>zz', opts)
+vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
+vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
 
+-- Resize windows arrows
+vim.keymap.set("n", "<Up>", ":resize -2<CR>", opts)
+vim.keymap.set("n", "<Down>", ":resize +2<CR>", opts)
+vim.keymap.set("n", "<Left>", ":vertical -2<CR>", opts)
+vim.keymap.set("n", "<Right>", ":vertical +2<CR>", opts)
 
+-- Buffers
+vim.keymap.set("n", "<Tab>", ":bnext<CR>", opts)
+vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", opts)
+vim.keymap.set("n", "<leader>x", ":bdelete!<CR>", opts)
+vim.keymap.set("n", "<leader>b", "<cmd> enew <CR>", opts)
+
+-- Window management
+vim.keymap.set("n", "<leader>v", "<C-w>v", opts) --split window vertically
+vim.keymap.set("n", "<leader>h", "<C-w>s", opts) --split window horizontally
+vim.keymap.set("n", "<leader>se", "<C-w>=", opts)
+vim.keymap.set("n", "<leader>xs", ":close<CR>", opts)
+
+-- Tabs
+vim.keymap.set("n", "<leader>to", ":tabnew<CR>", opts) -- open a new tab
+vim.keymap.set("n", "<leader>tx", ":tabclose<CR>", opts) -- close  current tab
+vim.keymap.set("n", "<leader>tn", ":tabn<CR>", opts) -- go to next tab
+vim.keymap.set("n", "<leader>tp", ":tabp<CR>", opts) -- go to previous tab
+
+-- Stay in indent mode
+vim.keymap.set("v", "<", "<gv", opts)
+vim.keymap.set("v", ">", ">gv", opts)
+
+-- Diagnostic keymaps
+vim.keymap.set("n", "S-k", vim.diagnostic.open_float, opts)
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,15 +1,19 @@
 vim.opt.number = true -- Make line number visible
 vim.opt.relativenumber = true -- Set relative numbered lines
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 vim.opt.showmode = false
 vim.opt.breakindent = true -- Enable break indent
-vim.opt.ignorecase = true -- 
+vim.opt.ignorecase = true --
 vim.opt.smartcase = true
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.autoindent = true
 vim.opt.linebreak = true
 vim.opt.tabstop = 4
 vim.opt.hlsearch = false
+vim.opt.undofile = true
 
+vim.schedule(function()
+	vim.opt.clipboard = "unnamedplus"
+end)
